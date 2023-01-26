7600841bd696b356e7e73d69fadd46617f20b754
ivan-block <ivancollev2@gmail.com>
2023-01-26 20:54:37 +0100

    build a book app

---
+++ book-app/src/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+
+function BookList() {
+  return (
+    <section className='booklist'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
+}
+
+function Book() {
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  )
+}
+
+function Image() {
+  return (
+    <img width={300} src='https://s.catch.com.au/images/product/0061/61001/61ad8752e6146275217490.jpg' alt='' />
+  )
+}
+
+function Title() {
+  return (
+    <h1>Love is in the wind</h1>
+  )
+}
+
+function Author() {
+  return (
+    <h4>Ivan Agwuye</h4>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BookList />
+);
